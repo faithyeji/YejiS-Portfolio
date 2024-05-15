c06451a7f64b8268102381592c07068b4c7ee3bb
Yeji Seo <faithyejiseo@gmail.com>
2024-05-15 16:10:08 -0400

    Accessibility improvements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,26 +4,40 @@ export default function Nav() {
       <div className="font-serif text-xl flex justify-between content-center items-center">
         <div className="flex gap-5">
           <div>
-            <a href="/playground">playground</a>
+            <a href="/playground" aria-label="Yeji's sandbox">
+              playground
+            </a>
           </div>
           <div>
             <a href="/about">about</a>
           </div>
         </div>
         <div>
-          <a href="/">
-            <img src="/yejiLogo.svg" className=" w-7 pb-2"></img>
+          <a href="/" aria-label="Yeji's portfolio home">
+            <img
+              src="/yejiLogo.svg"
+              className="w-7 pb-2"
+              alt="yeji seo logo"
+            ></img>
           </a>
         </div>
         <div>
           <div className="flex gap-3 text-lg">
             <div>
-              <a href="https://github.com/faithyeji" target="_blank">
+              <a
+                href="https://github.com/faithyeji"
+                target="_blank"
+                aria-label="Yeji Seo Github"
+              >
                 github
               </a>
             </div>
             <div>
-              <a href="https://read.cv/seoyemilk" target="_blank">
+              <a
+                href="https://read.cv/seoyemilk"
+                target="_blank"
+                aria-label="Yeji Seo CV"
+              >
                 CV
               </a>
             </div>
